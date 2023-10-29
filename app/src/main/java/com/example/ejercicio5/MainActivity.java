9ce5960beeb73e939bc52a979a4c8029fb46449b
saryta008 <saryta.93@hotmail.com>
2023-10-29 09:12:31 +0100

    Exercise finished

--- app/src/main/java/com/example/ejercicio5/MainActivity.java
+++ app/src/main/java/com/example/ejercicio5/MainActivity.java
@@ -4,15 +4,16 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.DialogFragment;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     ImageView off;
     ImageView on;
-    DialogFragment contenedor;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,14 +22,34 @@ public class MainActivity extends AppCompatActivity {
         off.setImageResource(R.drawable.llamadaoff);
         on=findViewById(R.id.llamadaon);
         on.setImageResource(R.drawable.llamadaon);
-        off.setOnClickListener(new View.OnClickListener() {
+        on.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                contenedor = new MyDialogFragment();
-                contenedor.show(getSupportFragmentManager(),"Se hizo clic en la imagen");
+                AlertDialog.Builder confirmarLlamada = new AlertDialog.Builder(MainActivity.this);
+                confirmarLlamada.setTitle("Has pulsado el botón de llamar");
+                confirmarLlamada.setMessage("¿Quieres llamar?");
+                confirmarLlamada.setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        on.setVisibility(View.INVISIBLE);
+                        off.setVisibility(View.VISIBLE);
+                        Toast.makeText(MainActivity.this,"Llamada en curso\nPulsa el botón para colgar",Toast.LENGTH_SHORT).show();
+                        off.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Toast.makeText(MainActivity.this,"Llamada Finalizada",Toast.LENGTH_SHORT).show();
+                                off.setVisibility(View.INVISIBLE);
+                                on.setVisibility(View.VISIBLE);
+                            }
+                        });
+                    }
+                });
+                confirmarLlamada.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(MainActivity.this,"Llamada cancelada",Toast.LENGTH_SHORT).show();
+                    }
+                });
+                confirmarLlamada.show();
             }
-
         });
-
     }
 }
